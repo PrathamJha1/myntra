f9f9d20e5622cca153c7f062d8c98a91aa8a0b46
Pratham <pratham@near.com>
2023-03-18 15:31:09 +0530

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Components/Auth/Login';
+import Signup from './Components/Auth/Signup';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Footer } from './Components/Footer/Footer';
+import { Home } from './Components/Dashboard/Home/Home';
+import { Navbar } from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/signup"
+          element={<Signup />}
+        ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
